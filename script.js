c0f0fe9fd4ec943cf7a26c8953118a3c3d09396f
johndoe <bmxartem@gmail.com>
2026-03-05 23:27:13 +0800

    commit2

--- script.js
+++ script.js
@@ -1,40 +1,95 @@
 const canvas = document.getElementById('canvas');
 const c = canvas.getContext('2d');
-
 canvas.width = 1024;
 canvas.height = 576;
-
 c.fillRect(0, 0, canvas.width, canvas.height);
+const gravity = 0.2;
+
 class Sprite {
   constructor({position, velocity}) {
     this.position = position;
     this.velocity = velocity;
+    this.height = 150;
+    this.width = 50;
   }
   draw() {
     c.fillStyle = 'red';
-    c.fillRect(this.position.x, this.position.y, 50, 150)
+    c.fillRect(this.position.x, this.position.y, this.width, this.height)
   }
   update() {
     this.draw();
-    this.position.y += 10;
+    this.position.y += this.velocity.y;
+    this.position.x += this.velocity.x;
+    if (this.position.y + this.height >= canvas.height) {
+      this.velocity.y = 0;
+    } else {
+      this.velocity.y += gravity;
+    }
   }
 }
+
 const player = new Sprite({
   position: { x: 0, y: 0 },
-  velocity: { x: 0, y: 0 }
+  velocity: { x: 0, y: 10 }
 });
+
 const enemy = new Sprite({
   position: { x: 400, y: 100},
   velocity: { x: 0, y: 0 }
 });
+
 player.draw();
 enemy.draw();
 
-
 function animate() {
   window.requestAnimationFrame(animate);
+  c.fillStyle = 'black';
+  c.fillRect(0, 0, canvas.width, canvas.height);
   player.update();
   enemy.update();
+
+  if (keys.a.pressed && lastKey === 'a') {
+    player.velocity.x = -1;
+  } else if (keys.d.pressed && lastKey === 'd') {
+    player.velocity.x = 1;
+  } else {
+    player.velocity.x = 0;
+  }
 }
 
+const keys = {
+  a: {
+    pressed: false
+  },
+  d: {
+    pressed: false
+  }
+}
+let lastKey = '';
+
 animate();
+window.addEventListener('keydown', (event) => {
+  switch (event.key) {
+    case 'd':
+      keys.d.pressed = true
+      lastKey = 'd';
+      break
+    case 'a':
+      keys.a.pressed = true
+      lastKey = 'a';
+      break
+    
+  }
+  console.log(event.key);
+})
+window.addEventListener('keyup', (event) => {
+  switch (event.key) {
+    case 'd':
+      keys.d.pressed = false
+      break
+    case 'a':
+      keys.a.pressed = false
+      break
+  }
+  console.log(event.key);
+})
